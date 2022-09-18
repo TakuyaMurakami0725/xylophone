49c79294029b13eb19cde8438625383bcba7428b
TakuyaMurakami0725 <got.dev.0725@gmail.com>
2022-09-18 21:59:13 +0900

    initial commit

--- Xylophone/ViewController.swift
+++ Xylophone/ViewController.swift
@@ -7,14 +7,42 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
 
+    var player: AVAudioPlayer?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
-
-
+    
+    @IBAction func keyPressed(_ sender: UIButton) {
+        print("tapped:\(sender)")
+        print("Title:\(String(describing: sender.currentTitle))")
+        guard let title = sender.currentTitle else { return }
+        playSound(soundName: title)
+    }
+    
+    func playSound(soundName: String) {
+        guard let url = Bundle.main.url(forResource: soundName, withExtension: "wav") else {
+            return
+        }
+        
+        do {
+            try AVAudioSession.sharedInstance().setCategory(.playback, mode: .default)
+            try AVAudioSession.sharedInstance().setActive(true)
+            
+            player = try AVAudioPlayer(contentsOf: url, fileTypeHint: AVFileType.mp3.rawValue)
+            
+            guard let player = player else { return }
+            
+            player.play()
+            
+        } catch let error {
+            print (error.localizedDescription)
+        }
+    }
     
 
 }
